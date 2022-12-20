cca5a36f1afb71f8b9d1ead41689bc53729bc1fa
asadbekturgunboyev <murodaka99@gmail.com>
2022-12-20 10:34:20 +0500

    set gradle

---
+++ app/src/main/java/com/example/jismoniysalomatlik/network/GetDataService.java
@@ -0,0 +1,12 @@
+package com.example.jismoniysalomatlik.network;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public class GetDataService {
+
+//    @GET("/load")
+
+}
